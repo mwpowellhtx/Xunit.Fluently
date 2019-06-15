51f7e3857cb3dc2114083f0bdb81aade3782d1da
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-15 15:37:43 -0400

    removed unnecessary strongly typed sameness assertions

--- src/xunit.fluently.assert/IdentityAsserts.cs
+++ src/xunit.fluently.assert/IdentityAsserts.cs
@@ -16,33 +16,11 @@ namespace Xunit
         /// <summary>
         /// Verifies that <paramref name="actual"/> Same as <paramref name="expected"/>.
         /// </summary>
-        /// <typeparam name="T">The type of the Value being inspected.</typeparam>
-        /// <param name="actual">The Actual value.</param>
-        /// <param name="expected">The Expected value.</param>
-        /// <returns>The <paramref name="actual"/> value following successful Assertion.</returns>
-        /// <see cref="Assert.Same"/>
-        public static T AssertSame<T>(this T actual, T expected)
-            => InvokeBinaryWithReturn((x, y) => Assert.Same(x, y), actual, expected);
-
-        /// <summary>
-        /// Verifies that <paramref name="actual"/> Same as <paramref name="expected"/>.
-        /// </summary>
         /// <param name="actual">The Actual value.</param>
         /// <param name="expected">The Expected value.</param>
         /// <returns>The <paramref name="actual"/> value following successful Assertion.</returns>
         /// <see cref="Assert.Same"/>
         public static object AssertNotSame(this object actual, object expected)
             => InvokeBinaryWithReturn(Assert.NotSame, actual, expected);
-
-        /// <summary>
-        /// Verifies that <paramref name="actual"/> Same as <paramref name="expected"/>.
-        /// </summary>
-        /// <typeparam name="T">The type of the Value being inspected.</typeparam>
-        /// <param name="actual">The Actual value.</param>
-        /// <param name="expected">The Expected value.</param>
-        /// <returns>The <paramref name="actual"/> value following successful Assertion.</returns>
-        /// <see cref="Assert.Same"/>
-        public static T AssertNotSame<T>(this T actual, T expected)
-            => InvokeBinaryWithReturn((x, y) => Assert.Same(x, y), actual, expected);
     }
 }
