d0ca62f3180cf2890081b9503b1432033ad50cad
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-15 15:36:55 -0400

    updated to utilize the unary invocations as well as in the expected shape

--- src/xunit.fluently.assert/NullAsserts.cs
+++ src/xunit.fluently.assert/NullAsserts.cs
@@ -6,17 +6,30 @@ namespace Xunit
         /// <summary>
         /// Verifies that the <paramref name="value"/> is Null.
         /// </summary>
+        /// <param name="value">The Value being inspected.</param>
+        /// <returns>The <paramref name="value"/> following successful Assertion.</returns>
+        /// <see cref="Assert.Null"/>
+        public static object AssertNull(this object value)
+            => InvokeUnaryWithReturn(Assert.Null, value);
+
+        /// <summary>
+        /// Verifies that the <paramref name="value"/> is Null.
+        /// </summary>
         /// <typeparam name="T">The type of the Value being inspected.</typeparam>
         /// <param name="value">The Value being inspected.</param>
         /// <returns>The <paramref name="value"/> following successful Assertion.</returns>
         /// <see cref="Assert.Null"/>
         public static T AssertNull<T>(this T value)
-            where T : class
-        {
-            Assert.Null(value);
-            // ReSharper disable once ExpressionIsAlwaysNull
-            return value;
-        }
+            => InvokeUnaryWithReturn(x => Assert.Null(x), value);
+
+        /// <summary>
+        /// Verifies that the <paramref name="value"/> is Not Null.
+        /// </summary>
+        /// <param name="value">The Value being inspected.</param>
+        /// <returns>The <paramref name="value"/> following successful Assertion.</returns>
+        /// <see cref="Assert.NotNull"/>
+        public static object AssertNotNull(this object value)
+            => InvokeUnaryWithReturn(Assert.NotNull, value);
 
         /// <summary>
         /// Verifies that the <paramref name="value"/> is Not Null.
@@ -26,10 +39,6 @@ namespace Xunit
         /// <returns>The <paramref name="value"/> following successful Assertion.</returns>
         /// <see cref="Assert.NotNull"/>
         public static T AssertNotNull<T>(this T value)
-            where T : class
-        {
-            Assert.NotNull(value);
-            return value;
-        }
+            => InvokeUnaryWithReturn(x => Assert.NotNull(x), value);
     }
 }
