ae2748001cfb0731e36e686a5c2b42bdd511cf44
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-15 15:35:42 -0400

    corrected or clarified a handful of invocations added the unary invocations

--- src/xunit.fluently.assert/BinaryInvocations.cs
+++ src/xunit.fluently.assert/BinaryInvocations.cs
@@ -4,21 +4,23 @@ using System.Collections.Generic;
 // ReSharper disable once IdentifierTypo
 namespace Xunit
 {
-    // ReSharper disable once CommentTypo
-    /// <summary>
-    /// Furnishes a set of useful Fluent <see cref="Assert"/> extension methods.
-    /// </summary>
-    /// <see cref="Assert"/>
-    /// <see cref="!:https://github.com/xunit/assert.xunit">Project is organized more or less analogous to the Xunit project.</see>
     public static partial class FluentlyExtensionMethods
     {
+        private static object InvokeBinaryWithReturn(Action<object, object> callback
+            , object actual, object expected)
+        {
+            callback.Invoke(expected, actual);
+            return actual;
+        }
+
         private static T InvokeBinaryWithReturn<T>(Action<T, T> callback, T actual, T expected)
         {
             callback.Invoke(expected, actual);
             return actual;
         }
 
-        private static T InvokeBinaryWithReturn<T, TExpected>(Action<TExpected, T> callback, T actual, TExpected expected)
+        private static TActual InvokeBinaryWithReturn<TActual, TExpected>(Action<TExpected, TActual> callback
+            , TActual actual, TExpected expected)
         {
             callback.Invoke(expected, actual);
             return actual;
@@ -27,52 +29,93 @@ namespace Xunit
         private static T InvokeBinaryWithReturn<T>(Func<T, T, T> callback, T actual, T expected)
             => callback.Invoke(expected, actual);
 
-        private static T InvokeBinaryWithReturn<T>(Action<T, T, IEqualityComparer<T>> callback, T actual, T expected, IEqualityComparer<T> comparer)
+        private static T InvokeBinaryWithReturn<T>(Action<T, T, IEqualityComparer<T>> callback
+            , T actual, T expected, IEqualityComparer<T> comparer)
         {
             callback.Invoke(expected, actual, comparer);
             return actual;
         }
 
         // ReSharper disable PossibleMultipleEnumeration
-        private static IEnumerable<T> InvokeBinaryWithReturn<T>(Action<IEnumerable<T>, IEnumerable<T>> callback, IEnumerable<T> actual, IEnumerable<T> expected)
+        private static IEnumerable<T> InvokeBinaryWithReturn<T>(Action<IEnumerable<T>, IEnumerable<T>> callback
+            , IEnumerable<T> actual, IEnumerable<T> expected)
         {
             callback.Invoke(expected, actual);
             return actual;
         }
 
-        private static IEnumerable<T> InvokeBinaryWithReturn<T>(Action<IEnumerable<T>, IEnumerable<T>, IEqualityComparer<T>> callback, IEnumerable<T> actual, IEnumerable<T> expected, IEqualityComparer<T> comparer)
+        private static IEnumerable<T> InvokeBinaryWithReturn<T>(Action<IEnumerable<T>, IEnumerable<T>, IEqualityComparer<T>> callback
+            , IEnumerable<T> actual, IEnumerable<T> expected, IEqualityComparer<T> comparer)
         {
             callback.Invoke(expected, actual, comparer);
             return actual;
         }
 
-        private static IEnumerable<T> InvokeCollectionWithReturn<T>(Action<T, IEnumerable<T>, IEqualityComparer<T>> callback, IEnumerable<T> collection, T expected, IEqualityComparer<T> comparer)
+        private static IEnumerable<T> InvokeCollectionWithReturn<T>(Action<T, IEnumerable<T>, IEqualityComparer<T>> callback
+            , IEnumerable<T> collection, T expected, IEqualityComparer<T> comparer)
         {
             callback.Invoke(expected, collection, comparer);
             return collection;
         }
 
-        private static IEnumerable<T> InvokeCollectionWithReturn<T>(Action<IEnumerable<T>, Predicate<T>> callback, IEnumerable<T> collection, Predicate<T> predicate)
+        private static IEnumerable<T> InvokeCollectionWithReturn<T>(Action<IEnumerable<T>, Predicate<T>> callback
+            , IEnumerable<T> collection, Predicate<T> predicate)
         {
             callback.Invoke(collection, predicate);
             return collection;
         }
 
-        private static IEnumerable<T> InvokeCollectionWithReturn<T>(Action<T, IEnumerable<T>> callback, IEnumerable<T> collection, T expected)
+        private static IEnumerable<T> InvokeCollectionWithReturn<T>(Action<T, IEnumerable<T>> callback
+            , IEnumerable<T> collection, T expected)
         {
             callback.Invoke(expected, collection);
             return collection;
         }
         // ReSharper restore PossibleMultipleEnumeration
 
-        private static string InvokeStringComparison(Action<string, string, StringComparison> callback, string actualString, string expectedSubstring, StringComparison comparison)
+        /// <summary>
+        /// A bare <see cref="string"/> Invocation.
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <param name="actualString"></param>
+        /// <param name="expectedSubstring"></param>
+        /// <returns><paramref name="actualString"/> following successful <paramref name="callback"/>.</returns>
+        private static string InvokeStringComparison(Action<string, string> callback
+            , string actualString, string expectedSubstring)
+        {
+            callback.Invoke(expectedSubstring, actualString);
+            return actualString;
+        }
+
+        /// <summary>
+        /// A <see cref="string"/> Invocation involving a <paramref name="comparison"/>.
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <param name="actualString"></param>
+        /// <param name="expectedSubstring"></param>
+        /// <param name="comparison"></param>
+        /// <returns><paramref name="actualString"/> following successful <paramref name="callback"/>.</returns>
+        private static string InvokeStringComparison(Action<string, string, StringComparison> callback
+            , string actualString, string expectedSubstring, StringComparison comparison)
         {
             callback.Invoke(expectedSubstring, actualString, comparison);
             return actualString;
         }
 
-        private static string InvokeStringComparison(Action<string, string, bool, bool, bool> callback, string actual, string expected
-            , bool ignoreCase = false, bool ignoreLineEndingDifferences = false, bool ignoreWhiteSpaceDifferences = false)
+        /// <summary>
+        /// <see cref="string"/> Invocation including additional comparison flags.
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <param name="actual"></param>
+        /// <param name="expected"></param>
+        /// <param name="ignoreCase"></param>
+        /// <param name="ignoreLineEndingDifferences"></param>
+        /// <param name="ignoreWhiteSpaceDifferences"></param>
+        /// <returns><paramref name="actual"/> following successful <paramref name="callback"/>.</returns>
+        private static string InvokeStringComparison(Action<string, string, bool, bool, bool> callback
+            , string actual, string expected, bool ignoreCase = false
+            , bool ignoreLineEndingDifferences = false
+            , bool ignoreWhiteSpaceDifferences = false)
         {
             callback.Invoke(expected, actual, ignoreCase, ignoreLineEndingDifferences, ignoreWhiteSpaceDifferences);
             return actual;
---
+++ src/xunit.fluently.assert/UnaryInvocations.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+// ReSharper disable once IdentifierTypo
+namespace Xunit
+{
+    public static partial class FluentlyExtensionMethods
+    {
+        /// <summary>
+        /// Unary Invocation which returns <paramref name="actual"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="callback"></param>
+        /// <param name="actual"></param>
+        /// <returns>The <paramref name="actual"/> value after successful <paramref name="callback"/>.</returns>
+        private static bool InvokeUnaryWithReturn(Action<bool> callback, bool actual)
+        {
+            callback.Invoke(actual);
+            return actual;
+        }
+
+        /// <summary>
+        /// Unary Invocation which returns <paramref name="actual"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="callback"></param>
+        /// <param name="actual"></param>
+        /// <returns>The <paramref name="actual"/> value after successful <paramref name="callback"/>.</returns>
+        private static bool? InvokeUnaryWithReturn(Action<bool?> callback, bool? actual)
+        {
+            callback.Invoke(actual);
+            return actual;
+        }
+
+        /// <summary>
+        /// Unary Invocation which returns <paramref name="actual"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="callback"></param>
+        /// <param name="actual"></param>
+        /// <returns>The <paramref name="actual"/> value after successful <paramref name="callback"/>.</returns>
+        private static T InvokeUnaryWithReturn<T>(Action<T> callback, T actual)
+        {
+            callback.Invoke(actual);
+            return actual;
+        }
+
+        /// <summary>
+        /// Unary Invocation which makes inferences between <typeparamref name="TActual"/>
+        /// and <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="TActual"></typeparam>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="callback"></param>
+        /// <param name="actual"></param>
+        /// <returns></returns>
+        private static T InvokeUnaryWithReturn<TActual, T>(Func<TActual, T> callback, TActual actual)
+            => callback.Invoke(actual);
+    }
+}
