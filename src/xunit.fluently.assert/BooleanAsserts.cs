5303fa6b7a9ffee4c553381f35f4ba628d8cf874
Michael W Powell <mwpowellhtx@gmail.com>
2019-06-17 12:43:39 -0400

    added a slightly more flexible unary invocation updated the Boolean assertions

--- src/xunit.fluently.assert/BooleanAsserts.cs
+++ src/xunit.fluently.assert/BooleanAsserts.cs
@@ -1,4 +1,7 @@
 ﻿// ReSharper disable once IdentifierTypo
+
+using System;
+
 namespace Xunit
 {
     public static partial class FluentlyExtensionMethods
@@ -38,5 +41,53 @@ namespace Xunit
         /// <see cref="Assert.True(bool?)"/>
         public static bool? AssertFalse(this bool? value)
             => InvokeUnaryWithReturn(Assert.False, value);
+
+        /// <summary>
+        /// Asserts whether <paramref name="actual"/> meets the <see cref="bool"/>
+        /// <paramref name="callback"/> condition.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="actual"></param>
+        /// <param name="callback"></param>
+        /// <returns></returns>
+        /// <see cref="Assert.True(bool)"/>
+        public static T AssertTrue<T>(this T actual, Func<T, bool> callback)
+            => InvokeUnaryWithReturn(Assert.True, actual, callback.Invoke);
+
+        /// <summary>
+        /// Asserts whether <paramref name="actual"/> meets the <see cref="bool"/>
+        /// <paramref name="callback"/> condition.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="actual"></param>
+        /// <param name="callback"></param>
+        /// <returns></returns>
+        /// <see cref="Assert.True(bool?)"/>
+        public static T AssertTrue<T>(this T actual, Func<T, bool?> callback)
+            => InvokeUnaryWithReturn(Assert.True, actual, callback.Invoke);
+
+        /// <summary>
+        /// Asserts whether <paramref name="actual"/> meets the <see cref="bool"/>
+        /// <paramref name="callback"/> condition.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="actual"></param>
+        /// <param name="callback"></param>
+        /// <returns></returns>
+        /// <see cref="Assert.False(bool)"/>
+        public static T AssertFalse<T>(this T actual, Func<T, bool> callback)
+            => InvokeUnaryWithReturn(Assert.False, actual, callback.Invoke);
+
+        /// <summary>
+        /// Asserts whether <paramref name="actual"/> meets the <see cref="bool"/>
+        /// <paramref name="callback"/> condition.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="actual"></param>
+        /// <param name="callback"></param>
+        /// <returns></returns>
+        /// <see cref="Assert.False(bool?)"/>
+        public static T AssertFalse<T>(this T actual, Func<T, bool?> callback)
+            => InvokeUnaryWithReturn(Assert.False, actual, callback.Invoke);
     }
 }
--- src/xunit.fluently.assert/UnaryInvocations.cs
+++ src/xunit.fluently.assert/UnaryInvocations.cs
@@ -55,5 +55,22 @@ namespace Xunit
         /// <returns></returns>
         private static T InvokeUnaryWithReturn<TActual, T>(Func<TActual, T> callback, TActual actual)
             => callback.Invoke(actual);
+
+        /// <summary>
+        /// Unary Invocation which returns <paramref name="actual"/>. In this case, we allow
+        /// an <typeparamref name="TInner"/> and a <typeparamref name="TOuter"/> value to inform
+        /// the Invocation.
+        /// </summary>
+        /// <typeparam name="TOuter"></typeparam>
+        /// <typeparam name="TInner"></typeparam>
+        /// <param name="callback"></param>
+        /// <param name="actual"></param>
+        /// <param name="innerCallback"></param>
+        /// <returns>The <paramref name="actual"/> value after successful <paramref name="callback"/>.</returns>
+        private static TInner InvokeUnaryWithReturn<TOuter, TInner>(Action<TOuter> callback, TInner actual, Func<TInner, TOuter> innerCallback)
+        {
+            callback.Invoke(innerCallback.Invoke(actual));
+            return actual;
+        }
     }
 }
