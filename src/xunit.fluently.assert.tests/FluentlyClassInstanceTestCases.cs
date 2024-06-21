7b2da31bec672a8ad58ccc3bdbb821c5dabc3757
Michael W. Powell <mwpowelllde@gmail.com>
2024-06-20 21:18:23 -0400

    Added unit tests around the positive and negative use cases. Positive cases that should pass. Negative cases that should also pass but in so doing did throw the expected exception.

---
+++ src/xunit.fluently.assert.tests/FluentlyClassInstanceTestCases.cs
@@ -0,0 +1,88 @@
+﻿namespace Xunit
+{
+    /// <inheritdoc/>
+    public class FluentlyClassInstanceTestCases : FluentlyClassInstanceTestCasesBase
+    {
+        /// <inheritdoc/>
+        public FluentlyClassInstanceTestCases()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public virtual void AssertsClassInstanceIsNull() =>
+            TestClass.NullInstance.AssertClassInstanceNull();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public virtual void AssertsClassInstanceIsNotNull() =>
+            TestClass.Instance.AssertClassInstanceNotNull();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public virtual void AssertsClassMemberNull() =>
+            TestClass.InstanceNullValue.AssertClassMemberNull(x => x.Value);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public virtual void AssertsClassMemberNotNull() =>
+            TestClass.Instance.AssertClassMemberNotNull(x => x.Value);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public virtual void AssertsClassMemberStructNull() =>
+            TestClass.InstanceNullValue.AssertClassMemberStructNull(x => x.Value);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public virtual void AssertsClassMemberStructNotNull() =>
+            TestClass.Instance.AssertClassMemberStructNotNull(x => x.Value);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public virtual void AssertsClassInstanceIsNotNullCanFluent() =>
+            TestClass.Instance.AssertClassInstanceNotNull().AssertIsType<TestClass>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public virtual void AssertsClassMemberNullCanFluent() =>
+            TestClass.InstanceNullValue.AssertClassMemberNull(x => x.Value).AssertIsType<TestClass>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public virtual void AssertsClassMemberNotNullCanFluent() =>
+            TestClass.Instance.AssertClassMemberNotNull(x => x.Value).AssertIsType<TestClass>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public virtual void AssertsClassMemberStructNullCanFluent() =>
+            TestClass.InstanceNullValue.AssertClassMemberStructNull(x => x.Value).AssertIsType<TestClass>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public virtual void AssertsClassMemberStructNotNullCanFluent() =>
+            TestClass.Instance.AssertClassMemberStructNotNull(x => x.Value).AssertIsType<TestClass>();
+    }
+}
